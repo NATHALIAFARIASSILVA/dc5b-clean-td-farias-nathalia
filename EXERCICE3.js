f7e1090ee965ce40517f8ae8c512a6b7b364c0b3
NATHALIA FARIAS <nathaliafarias1095@gmail.com>
2023-02-26 22:32:21 +0100

    envoi exo

--- EXERCICE3.js
+++ EXERCICE3.js
@@ -9,10 +9,7 @@ const writeStream = fs. createWriteStream ('path/to/new-file.csv');
 const parser = createCsvParser ();
 
 parser. transform = function (record, callback) {    
-    const cleanedRecord = record
-    .filter(field => field.trim() !== '') // Supprime les ligne vide 
-    .filter((_, index) => index !== 3) // supprime le 4e champs de l'enregistrement
-    .map(field => field.trim()); // Supprime les espaces inutiles
+
 
 if (cleanedRecord. length === 4) {
     callback(null, cleanedRecord);
